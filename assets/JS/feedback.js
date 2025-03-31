77ed02de5d4c008d077b38634e2c83053329caef
iTino07 <Sabatino.b007@outlook.it>
2025-03-31 23:04:36 +0200

    delete img in assets

--- assets/JS/feedback.js
+++ assets/JS/feedback.js
@@ -1,2 +1,12 @@
 // FORM
 
+const containerStar = () => {
+  const star = document.querySelector(".star");
+  const containerImg = document.createElement("img");
+  containerImg.src = "../star.svg";
+  containerImg.alt = "star";
+
+  star.appendChild(containerImg);
+};
+
+containerStar();
