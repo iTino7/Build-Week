bc429bd23d09c56bd1ffea48c9547421024f2f4e
DngDavide02 <dangelodavide02@gmail.com>
2025-03-31 19:32:20 +0200

    update css

--- assets/JS/result.js
+++ assets/JS/result.js
@@ -1,1 +1,8 @@
 //js result
+
+document.getElementById("correctAnswerSpan").innerHTML = "3/10";
+
+document.getElementById("wrongAnswerSpan").innerHTML = "7/10";
+
+document.getElementById("percBorder").innerHTML = "ciao";
+//   "congratulation! you passed the exam we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
