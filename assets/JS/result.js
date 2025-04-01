f2700e1562573e8eda60a2e06e550c7e7650a969
DngDavide02 <dangelodavide02@gmail.com>
2025-04-01 10:23:11 +0200

    java upgrade

--- assets/JS/result.js
+++ assets/JS/result.js
@@ -1,20 +1,27 @@
 //js result
-document.getElementById("correctAnswerSpan").innerHTML = "3/10";
 
-document.getElementById("wrongAnswerSpan").innerHTML = "7/10";
-
-const votiPositivi =
+const votiPositivi = localStorage.getItem("risposteGiuste");
+const votiTotali = localStorage.getItem("risposteTotali");
+const mediaTotale = localStorage.getItem("media");
 const votiNegativi = votiTotali - votiPositivi;
+const percentualePositiva = mediaTotale;
+const percentualeNegativa = 100 - mediaTotale;
 
-const votiTotali = localStorage.getItem("media");
-console.log(votiTotali);
+document.getElementById("percentualeCorretta").innerHTML = `${percentualePositiva}%`;
 
-/*
-if(voto > 60){
-document.getElementById("percBorder").innerHTML = "promosso";
-"congratulation! you passed the exam we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
-}else{
-document.getElementById("percBorder").innerHTML = "bocciato";
-"Unfortunately, you didn't pass the exam. we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
+document.getElementById("percentualeNegativa").innerHTML = `${percentualeNegativa}%`;
+
+document.getElementById("correctAnswerSpan").innerHTML = `${votiPositivi}/10`;
+
+document.getElementById("wrongAnswerSpan").innerHTML = `${votiNegativi}/10`;
+
+if (mediaTotale > 60) {
+  document.getElementById("percBorder").innerHTML =
+    "congratulation! you passed the exam we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
+} else {
+  document.getElementById("percBorder").innerHTML = "bocciato";
+  ("Unfortunately, you didn't pass the exam. we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)");
 }
-*/
+
+const circle = document.getElementById("percContainer");
+const percent = document.getElementById("percBorder");
