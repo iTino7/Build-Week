41fea7fac6b2ce8d1912d52bcc32d266cdf9a64c
DngDavide02 <dangelodavide02@gmail.com>
2025-04-01 09:20:03 +0200

    Update result.js

--- assets/JS/result.js
+++ assets/JS/result.js
@@ -4,5 +4,12 @@ document.getElementById("correctAnswerSpan").innerHTML = "3/10";
 
 document.getElementById("wrongAnswerSpan").innerHTML = "7/10";
 
-document.getElementById("percBorder").innerHTML = "ciao";
-//   "congratulation! you passed the exam we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
+/*
+if(voto > 60){
+document.getElementById("percBorder").innerHTML = "promosso";
+"congratulation! you passed the exam we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
+}else{
+document.getElementById("percBorder").innerHTML = "bocciato";
+"Unfortunately, you didn't pass the exam. we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
+}
+*/
