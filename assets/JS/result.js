24a9d4a11407a2468d07e45b1387b6d0d32d80bf
Ren-Mor <renatomorelli@hotmail.it>
2025-04-01 16:13:13 +0200

    end commit

--- assets/JS/result.js
+++ assets/JS/result.js
@@ -1,5 +1,4 @@
-//js result
-
+// prendi i dati
 const votiPositivi = localStorage.getItem("risposteGiuste");
 const votiTotali = localStorage.getItem("risposteTotali");
 const mediaTotale = localStorage.getItem("media");
@@ -7,25 +6,46 @@ const votiNegativi = votiTotali - votiPositivi;
 const percentualePositiva = mediaTotale;
 const percentualeNegativa = 100 - mediaTotale;
 
-document.getElementById(
-  "percentualeCorretta"
-).innerHTML = `${percentualePositiva}%`;
+// inserisci le percentuali nelle valutazioni
+document.getElementById("percentualeCorretta").innerHTML = `${percentualePositiva}%`;
+document.getElementById("percentualeNegativa").innerHTML = `${percentualeNegativa}%`;
 
-document.getElementById(
-  "percentualeNegativa"
-).innerHTML = `${percentualeNegativa}%`;
-
+// inserisci i voti nelle valutazioni
 document.getElementById("correctAnswerSpan").innerHTML = `${votiPositivi}/10`;
-
 document.getElementById("wrongAnswerSpan").innerHTML = `${votiNegativi}/10`;
 
+// vedere se sei primosso o bocciato
 if (mediaTotale > 60) {
-  document.getElementById("percBorder").innerHTML =
-    "congratulation! you passed the exam we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)";
+  document.getElementById("percBorder").innerHTML = `
+    <span class="bold">congratulation!</span> 
+    <br>
+    <span class="color">you passed the exam</span> 
+    <br>
+    <span class="lighter">we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)</span>`;
 } else {
-  document.getElementById("percBorder").innerHTML = "bocciato";
-  ("Unfortunately, you didn't pass the exam. we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)");
+  document.getElementById("percBorder").innerHTML = `
+  <span class="bold">Unfortunately</span>
+  <br>
+  <span class="color">you didn't pass the exam.</span>
+  <br>
+  <span class="lighter">we'll sent you the certificate in few minutes. Check your email (including promotions/spam folder)</span>`;
 }
 
-const circle = document.getElementById("percContainer");
-const percent = document.getElementById("percBorder");
+// riempi il cerchio dinamicamente
+const circle = document.querySelector("#perc-svg circle");
+const circonferenza = 251.2;
+const svgColor = circonferenza * (1 - percentualePositiva / 100);
+const svgColorWrong = circonferenza * (1 - percentualeNegativa / 100);
+
+circle.setAttribute("stroke-dasharray", circonferenza);
+circle.setAttribute("stroke-dashoffset", svgColor * -1);
+circle.setAttribute("transform", "rotate (270 50 50)");
+circle.style.transition = "stroke-dashoffset 1s ease-in-out";
+circle.style.stroke = "#00ffff";
+
+const circleWrong = document.querySelector("#perc-svg-wrong");
+circleWrong.setAttribute("stroke-dasharray", circonferenza);
+circleWrong.setAttribute("stroke-dashoffset", svgColorWrong);
+circleWrong.setAttribute("transform", "rotate (270 50 50)");
+circleWrong.style.transition = "stroke-dashoffset 1s ease-in-out";
+circleWrong.style.stroke = "#C2128D";
